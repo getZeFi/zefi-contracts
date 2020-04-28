ef9cc34e518cf35836a44326d80edc3ff705787c
srvtmr <sauravtom@icloud.com>
2020-04-28 15:20:52 -0700

    added deployment details for rinkeby network

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,22 @@
+
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+// idbravemac16 wallet's seed phrase
+const MNEMONIC = 'release exist stove life clock veteran coral tape rally danger other town'
+
 module.exports = {
   networks: {
     development: {
       host: '127.0.0.1',
       port: 8545,
       network_id: '*',
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(MNEMONIC, "https://rinkeby.infura.io/v3/741ef88b1e1d4a4d97268a379983c9f9")
+      },
+      network_id: '*',
+      gas: 4000000      //make sure this gas allocation isn't over 4M, which is the max
     }
   },
   compilers: {
