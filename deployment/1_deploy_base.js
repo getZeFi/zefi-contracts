eecf38a8d9c5606a5ba88f829178e30777431489
Lingraj Mahanand <lmahanand@gmail.com>
2020-08-10 10:45:44 +0530

    Multisig Wallet

---
+++ deployment/1_deploy_base.js
@@ -0,0 +1,10 @@
+const BaseWallet = require('../build/BaseWallet');
+const ModuleRegistry = require('../build/ModuleRegistry');
+const CompoundRegistry = require('../build/CompoundRegistry');
+const MultiSig = require('../build/ZefiMultiSigWallet');
+const WalletFactory = require('../build/ZefiWalletFactory');
+
+const utils = require('../utils/utilities.js');
+
+const DeployManager = require('../utils/deploy-manager.js');
+const MultisigExecutor = require('../utils/multisigexecutor.js');
